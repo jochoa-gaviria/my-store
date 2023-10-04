78a7e368abd2c7a226ecb2cb1fb23a5fc13ce453
Juan David Ochoa <david.ochoa@ceiba.com.co>
2023-10-04 16:13:11 -0500

    feat(data binding): property binding, ngModel, events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  buttonDisable: boolean = true;
+
+  person = {
+    name: "Juan",
+    age: 25,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton() {
+    this.buttonDisable = !this.buttonDisable
+  }
+
+  increaseAge() {
+    this.person.age++;
+  }
+
+  onBoxScroll(event: Event) {
+    const element = event.target as HTMLDivElement;
+    console.log(element.scrollTop);
+  }
+
+  onInputKeyUp(event: KeyboardEvent) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
